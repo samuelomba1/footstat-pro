d13f2166c93bd7eff84d475089edf67353c60db1
samuelomba1 <samuelomba96@gmail.com>
2026-06-14 04:16:45 +0200

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,4 @@
+export const metadata = { title: "FootStat Pro 2026", description: "Analyse football par IA" }
+export default function RootLayout({ children }) {
+  return <html lang="fr"><body style={{margin:0}}>{children}</body></html>
+}
