a68171b3f13e4db4f09b09a05de4aa9815481a79
samuelomba1 <samuelomba96@gmail.com>
2026-06-14 04:09:18 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: { domains: ["media.api-sports.io", "media-1.api-sports.io", "media-2.api-sports.io"] }
+}
+module.exports = nextConfig
